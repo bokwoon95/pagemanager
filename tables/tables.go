8a4a1b3332da8123f96a6e25c6a8df4fff259982
bokwoon <bokwoon.c@gmail.com>
2021-09-21 18:27:49 +0800

    checkpoint

--- tables/tables.go
+++ tables/tables.go
@@ -3,5 +3,64 @@ package tables
 import "github.com/bokwoon95/pagemanager/sq"
 
 type PM_SITE struct {
-	sq.TableInfo
+	sq.TableInfo `ddl:"unique={. cols=domain,subdomain}"`
+	SITE_ID      sq.UUIDField `ddl:"primarykey"`
+	DOMAIN       sq.StringField
+	SUBDOMAIN    sq.StringField
+}
+
+type PM_ROUTE struct {
+	sq.TableInfo `ddl:"primarykey={. cols=site_id,url_path}"`
+	SITE_ID      sq.UUIDField
+	URL_PATH     sq.StringField
+	PLUGIN       sq.StringField
+	PARAMS       sq.JSONField
+}
+
+type PM_TEMPLATE_DATA struct {
+	sq.TableInfo `ddl:"primarykey={. cols=site_id,langcode,data_file}"`
+	SITE_ID      sq.UUIDField
+	LANGCODE     sq.StringField
+	DATA_FILE    sq.StringField
+	DATA         sq.JSONField
+}
+
+type PM_USER struct {
+	sq.TableInfo  `ddl:"primarykey={. cols=user_id}"`
+	USER_ID       sq.UUIDField
+	USERNAME      sq.StringField
+	EMAIL         sq.StringField `ddl:"unique"`
+	NAME          sq.StringField `ddl:"unique"`
+	PASSWORD_HASH sq.StringField
+}
+
+type PM_USER_AUTHZ struct {
+	sq.TableInfo          `ddl:"primarykey={. cols=site_id,user_id}"`
+	SITE_ID               sq.UUIDField
+	USER_ID               sq.UUIDField
+	ROLES                 sq.CustomField `ddl:"type=JSON postgres:type=TEXT[]"`
+	AUTHZ_ATTRIBUTES      sq.JSONField
+	ROLE_AUTHZ_ATTRIBUTES sq.JSONField
+}
+
+type PM_USER_AUTHZ_ROLES_TBLIDX struct {
+	sq.TableInfo `ddl:"ignore=postgres,mysql primarykey={. cols=site_id,user_id,role}"`
+	SITE_ID      sq.UUIDField
+	USER_ID      sq.UUIDField
+	ROLE         sq.StringField
+}
+
+type PM_ROLE struct {
+	sq.TableInfo     `ddl:"ignore=postgres,mysql primarykey={. cols=site_id,role}"`
+	SITE_ID          sq.UUIDField
+	ROLE             sq.StringField
+	AUTHZ_ATTRIBUTES sq.JSONField
+}
+
+type PM_SESSION struct {
+	sq.TableInfo `ddl:"primarykey={. cols=session_hash}"`
+	SESSION_HASH sq.BlobField
+	SITE_ID      sq.UUIDField
+	USER_ID      sq.UUIDField
+	DATA         sq.JSONField
 }
